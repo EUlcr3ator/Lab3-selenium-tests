81857107c9fe2c17f24c78a49c4a7d74c929665e
EUlcr3ator <140016612+EUlcr3ator@users.noreply.github.com>
2025-05-25 16:12:06 +0300

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -9,7 +9,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 def test_successful_login():
 
     driver = webdriver.Chrome(service=ChromeService())
-    driver.maximize_window()
 
 
     driver.get("https://opensource-demo.orangehrmlive.com/")
